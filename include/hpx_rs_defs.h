aa395f2e06bc5dd02d6b68a8e6bf94cceae955a8
Shreyas Atre <shreyasatre16@gmail.com>
2024-04-01 21:14:04 -0500

    Fix hpx functions not having freedom to run on non hpx thread
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- include/hpx_rs_defs.h
+++ include/hpx_rs_defs.h
@@ -1,7 +1,7 @@
 #include <hpx/chrono.hpp>
 #include <hpx/format.hpp>
 #include <hpx/future.hpp>
-#include <hpx/init.hpp>
+#include <hpx/hpx_start.hpp>
 
 #include <cstdint>
 #include <hpx/iostream.hpp>
@@ -11,13 +11,9 @@ template <typename T> T fibonacci(T n);
 
 std::uint64_t fibonacci_hpx(std::uint64_t n);
 
-inline std::int32_t init() {
-  return hpx::init(
-      [](int argc, char *argv[]) {
-        hpx::cout << "fib (hpx) (10): " << fibonacci_hpx(10) << "\n";
-        return hpx::finalize();
-      },
-      0, nullptr);
-}
+inline std::int32_t start() { return hpx::start(nullptr, 0, nullptr); }
 
-inline std::int32_t finalize() { return hpx::finalize(); }
+inline std::int32_t stop() {
+  hpx::post([]() { hpx::finalize(); });
+  return hpx::stop();
+}
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,23 @@ mod ffi {
         fn fibonacci(u: u64) -> u64;
         fn fibonacci_hpx(u: u64) -> u64;
 
-        fn init() -> i32;
-        fn finalize() -> i32;
-
+        fn start() -> i32;
+        fn stop() -> i32;
 
     }
+}
+
+fn your_main() -> i32 {
+    let fib_hpx = ffi::fibonacci_hpx(10);
+    println!("fib hpx(10) = {:?}", fib_hpx);
+    return 0;
 }
 
 fn main() {
     let fib = ffi::fibonacci(10);
     println!("fib (non-hpx) (10) = {:?}", fib);
 
-    let _a = ffi::init();
-    // let fib_hpx = ffi::fibonacci_hpx(10);
-    // println!("fib hpx(10) = {:?}", fib_hpx);
-    // let _b = ffi::finalize();
+    let _a = ffi::start();
+    let _c = your_main();
+    let _b = ffi::stop();
 }
