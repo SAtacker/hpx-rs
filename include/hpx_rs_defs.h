fe2589460a6b5424c382518da259b088400b6b36
Shreyas Atre <shreyasatre16@gmail.com>
2024-04-01 20:55:08 -0500

    Fix HPX not finalizing on HPX thread and fix build script
    - Status: Working on Linux
    
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- include/hpx_rs_defs.h
+++ include/hpx_rs_defs.h
@@ -4,8 +4,20 @@
 #include <hpx/init.hpp>
 
 #include <cstdint>
+#include <hpx/iostream.hpp>
 #include <iostream>
 
 template <typename T> T fibonacci(T n);
 
 std::uint64_t fibonacci_hpx(std::uint64_t n);
+
+inline std::int32_t init() {
+  return hpx::init(
+      [](int argc, char *argv[]) {
+        hpx::cout << "fib (hpx) (10): " << fibonacci_hpx(10) << "\n";
+        return hpx::finalize();
+      },
+      0, nullptr);
+}
+
+inline std::int32_t finalize() { return hpx::finalize(); }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ mod ffi {
         fn fibonacci(u: u64) -> u64;
         fn fibonacci_hpx(u: u64) -> u64;
 
+        fn init() -> i32;
+        fn finalize() -> i32;
+
+
     }
 }
 
@@ -14,6 +18,8 @@ fn main() {
     let fib = ffi::fibonacci(10);
     println!("fib (non-hpx) (10) = {:?}", fib);
 
-    let fib_hpx = ffi::fibonacci_hpx(10);
-    println!("fib hpx(10) = {:?}", fib_hpx);
+    let _a = ffi::init();
+    // let fib_hpx = ffi::fibonacci_hpx(10);
+    // println!("fib hpx(10) = {:?}", fib_hpx);
+    // let _b = ffi::finalize();
 }
